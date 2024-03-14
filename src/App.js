540e0c658b4fb2672aa53d9b394448c82e396666
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-14 11:31:51 +0430

    init

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading]  = useState(false)
   return <h2>tabs project setup</h2>
 }
 
